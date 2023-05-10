94e2c11d1ef56b7a3f5c31efcc348cee0e7aa0c4
arbinydv <sominyadav7@gmail.com>
2023-05-10 22:57:34 +0545

    Date and Time formatting in Go

---
+++ 05Mytime/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Exploring Time pacakge in GO....")
+
+	// present time
+
+	presentTime := time.Now()
+
+	fmt.Println("Current time: ", presentTime)
+
+	// Formatting time based on user preferences
+	fmt.Println(presentTime.Format("01-02-2006 Monday"))
+
+	// Formatting Date time and Day as well
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	//creating custom date and then formatting them to make it look cleaner
+
+	customDate := time.Date(2023, time.December, 10, 20, 20, 20, 0, time.UTC)
+
+	fmt.Println(customDate)
+
+	//formatting it to look more clean
+
+	fmt.Println(customDate.Format("01-02-2006 15:04:05 Monday"))
+
+}
