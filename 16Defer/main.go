30ce2845a3222f99771cb9fd6c736e430d94178d
arbinydv <sominyadav7@gmail.com>
2023-05-23 23:27:54 +0545

    defer basics in go

--- 16Defer/main.go
+++ 16Defer/main.go
@@ -8,4 +8,14 @@ func main() {
 	fmt.Println("Hello ")           // this will get printed in normal fashion
 	defer fmt.Println("Kubernetes") // first in last out
 	defer fmt.Println("Golang")     // last in first out
+
+	// reverse print showing defer example in use
+
+	values := []int{1, 2, 3, 4, 5}
+
+	for i, _ := range values {
+		defer fmt.Printf("Number is: %v\n", i)
+	}
+
+	// Defer works in LIFO order
 }
